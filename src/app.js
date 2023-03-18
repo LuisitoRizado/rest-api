e11d94d0047025c023cd18ec538974af70fa0ffc
Luis Olivares <laog.sonic4@gmail.com>
2023-03-18 09:17:06 -0600

    probando

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ import {PORT} from './config.js'
 
 const app = express()
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+/* const bodyParser = require('body-parser');
+app.use(bodyParser.json()); */
 
 app.get('/', async (req, res) => {
   const [rows] = await pool.query('SELECT * FROM users')
