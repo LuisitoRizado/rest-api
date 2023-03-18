6098339f2b9440d97ae44d5a1b6eec6a7c3e27cd
Luis Olivares <laog.sonic4@gmail.com>
2023-03-17 23:10:56 -0600

    probando

--- src/app.js
+++ src/app.js
@@ -13,6 +13,16 @@ app.get('/ping', async (req, res) => {
   const [result] = await pool.query(`SELECT "hello world" as RESULT`);
   res.json(result[0])
 })
+app.get('/getAllHorarios', async (req, res) => {
+  const sql = "SELECT * FROM Horario";
+  const [result] = await pool.query(sql);
+  const users = result.map(user => ({
+    Id_Horario: user.Id_Horario,
+    Hora_Inicio_Lunes: user.Hora_Inicio_Lunes,
+    Hora_Final_Lunes: user.Hora_Final_Lunes
+  }));
+  res.json(users);
+});
 
 app.get('/create', async (req, res) => {
   const result = await pool.query('INSERT INTO users(name) VALUES ("John")')
