fa730f1a5f582cf29c68aa83d47ad4ee41362a32
Luis Olivares <laog.sonic4@gmail.com>
2023-03-25 09:36:58 -0600

    Finished

--- src/app.js
+++ src/app.js
@@ -834,7 +834,7 @@ app.delete('/deleteADocente/:ID_DOCENTE', async (req, res) => {
 });
 app.get('/getMaterias_docente/:id', async (req, res) => {
   const { id } = req.params;
-  const sql = "SELECT Materia_Asignada_Profesor.Id_DocxMath, Materia_Asignada_Profesor.Id_Docente, Materia_Asignada_Profesor.Id_Materia, Materia.Materia FROM Materia_Asignada_Profesor JOIN Materia ON Materia_Asignada_Profesor.Id_Materia = Materia.Id_Materia WHERE Materia_Asignada_Profesor.Id_Docente = ?";
+  const sql = "SELECT * FROM Materia ON Materia_Asignada_Profesor.Id_Materia = Materia.Id_Materia WHERE Materia_Asignada_Profesor.Id_Docente = ?";
   const [result] = await pool.query(sql, [id]);
 
   if (result.length === 0) {
@@ -843,10 +843,10 @@ app.get('/getMaterias_docente/:id', async (req, res) => {
   }
 
   const userSchema = {
-    "Id_DocxMath": result.Id_DocxMath,
-    "Id_Docente": result.Id_Docente,
-    "Id_Materia": result.Id_Materia,
-    "Materia":result.Materia
+    "Id_DocxMath": result[0].Id_DocxMath,
+    "Id_Docente": result[0].Id_Docente,
+    "Id_Materia": result[0].Id_Materia,
+    "Materia":result[0].Materia
   };
   let data = []
   data.push(userSchema);
