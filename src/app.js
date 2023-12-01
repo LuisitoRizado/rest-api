cd0f4c6a4f961138f39028238b2f4db894cd2d71
LuisitoRizado <laog.sonic4@gmail.com>
2023-11-30 22:34:36 -0600

    adding

--- src/app.js
+++ src/app.js
@@ -255,7 +255,7 @@ app.post('/addCarga', async (req, res) => {
 app.get('/getCarga/:NCONTROL', async (req, res) => {
   const { NCONTROL } = req.params;
   const sql = `
-  SELECT Materia.Materia AS Nombre_Materia, Materia.Semestre, Docente.Nombre AS Nombre_Docente, Materia_Cargada_Alumno.Calificacion
+  SELECT Materia.Materia AS Nombre_Materia, Materia.Semestre, Docente.Nombre AS Nombre_Docente, Materia_Cargada_Alumno.Calificacion,
   Aula.Nombre AS Nombre_Aula, Horas.Hora_Inicio, Horas.Hora_Final, Alumnos.Nombre AS Nombre_Alumno, Alumnos.Ap_Paterno, Alumnos.Ap_Materno
   FROM Alumnos
   INNER JOIN Materia_Cargada_Alumno ON Alumnos.NControl = Materia_Cargada_Alumno.NControl_Alumno
